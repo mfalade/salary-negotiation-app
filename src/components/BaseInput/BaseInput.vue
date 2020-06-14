31b0de2826cc05bf95b6cd5690f8beb3d907fe7c
Mayowa Falade <falademayowa240@gmail.com>
2020-06-14 23:29:54 +0100

    [feat]: implement base input component

---
+++ src/components/BaseInput/BaseInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label>
+      <p>{{ label }}</p>
+      <input
+        v-bind="$attrs"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+      />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseInput',
+  inheritAttrs: false,
+  props: {
+    label: {
+      required: true,
+    },
+    value: [String, Number],
+  },
+};
+</script>
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <main>
+    <BaseInput
+      v-model.number="employee.minExpectation"
+      label="Please enter your minimum expectation"
+      placeholder=""
+      type="number"
+      min="0"
+    />
+    <BaseInput
+      v-model.number="employer.maxOffer"
+      label="Please enter your maximum offer"
+      placeholder=""
+      type="number"
+      min="0"
+    />
+  </main>
+</template>
+
+<script>
+import BaseInput from '@/components/BaseInput/BaseInput.vue';
+
+export default {
+  name: 'HomeView',
+  components: { BaseInput },
+  data() {
+    return {
+      employee: {
+        minExpectation: '',
+        formSubmitted: false,
+      },
+      employer: {
+        maxOffer: '',
+        formSubmitted: false,
+      },
+    };
+  },
+};
+</script>
+
+<style src="./Home.scss" lang="scss" scoped></style>
